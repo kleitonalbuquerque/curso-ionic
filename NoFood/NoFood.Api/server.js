3c621f2b973c52f5899e60080d6385b46ba6f8df
kleitonalbuquerque <kleiton2102@gmail.com>
2019-08-14 00:28:35 -0300

    Node modules

--- NoFood/NoFood.Api/server.js
+++ NoFood/NoFood.Api/server.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+let pessoas = []
+
 app.get('/', (req, res) => {
-  res.status(200).send('Olá Mundo!')
+  res.status(200).send(pessoas)
+})
+
+app.post('/', (req, res) => {
+  pessoas.push(req.body)
+  res.status(201).send(req.body)
+})
+
+app.put('/:id', (req, res) => {
+  let pessoaEncontrada = pessoas.filter(pes => { return pes.id == req.params.id })
+  pessoaEncontrada =req.body
+  res.status(202).send(pessoaEncontrada)
+})
+
+app.delete('/:id', (req, res) => {
+  for (let index = 0; index < pessoas.length; index++) {
+    const pessoa = pessoas[index];
+    if  (pessoa.id == req.params.id) {
+      pessoas.splice(index, 1)
+    }   
+  }
+  res.status(204).send('Registro excluído')
 })
 
 app.listen(3000, () => {
