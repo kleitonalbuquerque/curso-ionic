4e422f918406247d1d3b3823c0a7d8ec49e12227
kleitonalbuquerque <kleiton2102@gmail.com>
2019-08-22 23:57:24 -0300

    Substituição da url do banco de dados, criação do repositório categorias e produtos e rafatoramento dos controllers

--- NoFood/NoFood.Api/bin/config/variables.js
+++ NoFood/NoFood.Api/bin/config/variables.js
@@ -3,7 +3,7 @@ const variables = {
     port: process.env.port || 3000
   },
   Database: {
-    connection: process.env.connection || 'mongodb+srv://admin:abcd1234@cluster0-6wyin.mongodb.net/test?retryWrites=true&w=majority'
+    connection: process.env.connection || 'mongodb+srv://admin:abcd1234@cluster0-6wyin.mongodb.net/nofood'
   }
 }
 
---
+++ NoFood/NoFood.Api/repositories/categoria-repository.js
@@ -0,0 +1,36 @@
+require('../models/categoria-model')
+const mongoose = require('mongoose')
+const CategoriaModel = mongoose.model('Categoria')
+
+class CategoriaRepository {
+  constructor() {
+
+  }
+
+  async create(data) {
+    let categoria = new CategoriaModel(data)
+    let resultado = await categoria.save()
+    return resultado
+  }
+
+  async update(id, data) {
+    await CategoriaModel.findByIdAndUpdate(id, { $set: data })
+    let resultado = await CategoriaModel.findById(id)
+    return resultado
+  }
+
+  async getAll() {
+    return await CategoriaModel.find()
+  }
+
+  async getById(id) {
+    return await CategoriaModel.findById(id)
+  }
+
+  async delete(id) {
+    return await CategoriaModel.findByIdAndRemove(id)
+  }
+
+}
+
+module.exports = CategoriaRepository
