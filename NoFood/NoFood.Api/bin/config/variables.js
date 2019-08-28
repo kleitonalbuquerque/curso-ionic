f448c657c95ef268b6763aa42bd23bb91b2e3a35
kleitonalbuquerque <kleiton2102@gmail.com>
2019-08-28 01:44:41 -0300

    Autenticação de usuário com jwt

--- NoFood/NoFood.Api/bin/config/variables.js
+++ NoFood/NoFood.Api/bin/config/variables.js
@@ -4,6 +4,9 @@ const variables = {
   },
   Database: {
     connection: process.env.connection || 'mongodb+srv://admin:abcd1234@cluster0-6wyin.mongodb.net/nofood'
+  },
+  Security: {
+    secretKey: 'd41d8cd98f00b204e9800998ecf8427e|ad49ed070ac42a479e78600215917b6c'
   }
 }
 
--- NoFood/NoFood.Api/controllers/usuario-controller.js
+++ NoFood/NoFood.Api/controllers/usuario-controller.js
@@ -5,6 +5,8 @@ const validation = require('../bin/helpers/validation')
 const ctrlBase = require('../bin/base/controller-base')
 const _repo = new repository()
 const md5 = require('md5')
+const jwt = require('jsonwebtoken')
+const variables = require('../bin/config/variables')
 
 function usuarioController() {
   
@@ -60,6 +62,31 @@ usuarioController.prototype.getById = async (req, res) => {
 
 usuarioController.prototype.delete = async (req, res) => {
   ctrlBase.delete(_repo, req, res)
+}
+
+usuarioController.prototype.autenticar = async (req, res) => {
+  let _validationContract = new validation()
+
+  _validationContract.isRequired(req.body.email, 'Informe seu email')
+  _validationContract.isEmail(req.body.email, 'Email inválido')
+  _validationContract.isRequired(req.body.senha, 'Informe sua senha')
+
+  if(!_validationContract.isValid()) {
+    res.status(400).send({ message: 'Não foi possível fazer o login', validation: _validationContract.errors() })
+    return
+  }
+
+  let usuarioEncontrado = await _repo.authenticate(req.body.email, req.body.senha)
+
+  if(usuarioEncontrado) {
+    res.status(200).send({
+      usuario: usuarioEncontrado,
+      token: jwt.sign({ user: usuarioEncontrado }, variables.Security.secretKey)
+    })
+  }else{
+    res.status(404).send({ message: 'Usuário e senha inválidos' })
+  }
+
 }
 
 module.exports = usuarioController
